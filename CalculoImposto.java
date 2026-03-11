171f903cc5aae451682e0bfb831edb45c89249f0
Castelo001 <161105347+Castelo001@users.noreply.github.com>
2026-03-11 23:26:12 +0000

    CalculoImposto

---
+++ CalculoImposto.java
@@ -0,0 +1,38 @@
+public class CalculoImposto {
+import java.util.Scanner;
+
+
+
+public static void main(String[] args) {
+     Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a quantidade de horas trabalhadas: ");
+        double qtdHoras = entrada.nextDouble();
+         
+        System.out.println("Digite o salario por horaa: ");
+        double salarioHora = entrada.nextDouble();
+   
+      System.out.println("Digite o número de dependentes: ");
+        int nmrDependentes = entrada.nextInt();
+
+        double salarioBruto =   qtdHoras * salarioHora +(50*nmrDependentes);
+double inss;
+         if (salarioBruto<=1000){
+              inss = salarioBruto*(8.5/100);
+         }else{
+             inss = salarioBruto *(9/100);
+         }
+    double impostoRenda;
+        if (salarioBruto<=500){
+            impostoRenda = 0;
+        }
+        if (salarioBruto>500 && salarioBruto<=1000){
+            impostoRenda = salarioBruto*(5/100);
+        }
+        if (salarioBruto>1000){
+            impostoRenda = salarioBruto*(7/100);
+        }
+        double salarioLiquido = salarioBruto - inss - impostoRenda;
+    System.out.println("O salário líquido é: "+ salarioLiquido);
+    }
+}
+
--- calculadora.java
+++ calculadora.java
@@ -1,1 +1,11 @@
-exercicio tabuada
+import javax.swing.JOptionPane;
+
+class Main {
+    public static void main(String[] args) {
+     int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero de 1 a 10 para visualisar tabuada: "));
+        int i;
+        for (i=0;i<=10 ;i++ ) {
+            System.out.println(num + "*"+i+"="+num*i);
+        }
+    }
+}
